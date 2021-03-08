5c220b543c2fe04cee513cb55600b525f088f9c8
Davin <davin.hayden@outlook.com>
2021-03-08 23:54:52 +0000

    created query to show all questions & points for each answer option

--- corelife_db_queries_v1.sql
+++ corelife_db_queries_v1.sql
@@ -4,10 +4,18 @@ INNER JOIN users AS u
 	ON u.user_id = ucs.user_id;
 	
 # show user score w date & q_id & points for answer & user name
+# do i need to knwo the points label as well as the amount of points?
 SELECT * FROM user_core_score AS ucs
 INNER JOIN user_core_answers AS uca
 	ON uca.ucs_id = ucs.ucs_id
 INNER JOIN users AS u
 	ON u.user_id = ucs.user_id;
 	
+# create views instead!	
 # still need a way to auto calculate the points for each answer
+# show questions and points for each answer option
+SELECT  rcq.q_id, rcq.question, rcq.points_type, rcp.pointsA_not, rcp.pointsB_only, rcp.pointsC_sometimes, rcp.pointsD_often, rcp.pointsE_most
+FROM ref_core_questions AS rcq
+INNER JOIN ref_core_points AS rcp
+ON rcq.points_type = rcp.points_id
+ORDER BY rcq.gp_order;
